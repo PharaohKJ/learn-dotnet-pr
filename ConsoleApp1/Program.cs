4e25dfa45c3cbc772997a67e338b759fa5224f62
Masayuki Katoh <kato@phalanxware.com>
2025-07-15 12:28:29 +0900

    足し算と引き算をできるようにした

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,3 +8,10 @@ string? input2 = Console.ReadLine();
 int number2 = int.Parse(input2 ?? "0");
 
 Console.WriteLine($"入力された値: {number1}, {number2}");
+
+// 足し算と引き算の計算
+int sum = number1 + number2;
+int difference = number1 - number2;
+
+Console.WriteLine($"足し算の結果: {number1} + {number2} = {sum}");
+Console.WriteLine($"引き算の結果: {number1} - {number2} = {difference}");
