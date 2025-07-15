122f1cc66be235667af815334c39003add9395c6
Masayuki Katoh <kato@phalanxware.com>
2025-07-15 12:11:39 +0900

    1st version

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("1つ目の数値を入力してください:");
+string? input1 = Console.ReadLine();
+int number1 = int.Parse(input1 ?? "0");
+
+Console.WriteLine("2つ目の数値を入力してください:");
+string? input2 = Console.ReadLine();
+int number2 = int.Parse(input2 ?? "0");
+
+Console.WriteLine($"入力された値: {number1}, {number2}");
